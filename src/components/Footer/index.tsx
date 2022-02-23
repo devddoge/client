11db172820f82b2115d5164a78a9b92b3d274ddc
huquduy <huquduy@gmail.com>
2022-02-23 12:58:12 +0700

    Update logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,3 @@
-import Dc from 'assets/images/Icon_DC.png'
-import Fb from 'assets/images/Icon_FB.png'
-import Rd from 'assets/images/Icon_RD.png'
 import Tw from 'assets/images/Icon_TW.png'
 import { Box, Flex } from 'rebass'
 import styled from 'styled-components/macro'
@@ -19,17 +16,8 @@ export default function Footer() {
   return (
     <FooterStyled width="100%" height="158px" backgroundColor="#1E4050">
       <Flex height="100%" justifyContent="center" alignItems="center">
-        <Link>
-          <img src={Fb} alt="fb" />
-        </Link>
-        <Link>
-          <img src={Rd} alt="fb" />
-        </Link>
         <Link href="https://twitter.com/DDoge_Coin" target="_blank">
           <img src={Tw} alt="fb" />
-        </Link>
-        <Link>
-          <img src={Dc} alt="fb" />
         </Link>
       </Flex>
     </FooterStyled>
